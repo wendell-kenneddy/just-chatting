3ff2639602f32b4c368de9fe8b314ca999a21936
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-11-09 07:47:21 -0300

    implement lazy loading of routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,34 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { AuthContextProvider } from './contexts/auth';
-import { NewRoom } from './pages/NewRoom';
-import { Dashboard } from './pages/Dashboard';
-import { JoinRoom } from './pages/JoinRoom';
-import { Landing } from './pages/Landing';
-import { Room } from './pages/Room';
-import { NoRouteMatch } from './components/NoRouteMatch';
-import { Toaster } from 'react-hot-toast';
 import { LoginInfoContextProvider } from './contexts/loginInfo';
+
+import { NoRouteMatch } from './components/NoRouteMatch';
+import { LoadingScreen } from './components/LoadingScreen';
+import { Toaster } from 'react-hot-toast';
+
+const NewRoom = lazy(() => import('./pages/NewRoom'));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const JoinRoom = lazy(() => import('./pages/JoinRoom'));
+const Landing = lazy(() => import('./pages/Landing'));
+const Room = lazy(() => import('./pages/Room'));
 
 export function App() {
   return (
     <LoginInfoContextProvider>
       <AuthContextProvider>
-        <Toaster />
         <BrowserRouter>
-          <Switch>
-            <Route path="/" exact component={Landing} />
-            <Route path="/dashboard" exact component={Dashboard} />
-            <Route path="/room/create" exact component={NewRoom} />
-            <Route path="/room/enter" exact component={JoinRoom} />
-            <Route path="/room/:id" exact component={Room} />
-            <Route path="*" exact component={NoRouteMatch} />
-          </Switch>
+          <Toaster />
+          <Suspense fallback={<LoadingScreen />}>
+            <Switch>
+              <Route path="/" exact component={Landing} />
+              <Route path="/dashboard" exact component={Dashboard} />
+              <Route path="/room/create" exact component={NewRoom} />
+              <Route path="/room/enter" exact component={JoinRoom} />
+              <Route path="/room/:id" exact component={Room} />
+              <Route path="*" exact component={NoRouteMatch} />
+            </Switch>
+          </Suspense>
         </BrowserRouter>
       </AuthContextProvider>
     </LoginInfoContextProvider>
